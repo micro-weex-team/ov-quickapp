77ac911e81f06791716802441f0aebf0e9d1da2c
unknown <ZHOUHG1@cn.midea.com>
2019-01-15 15:30:42 +0800

    第一次快应用上传

---
+++ README.md
@@ -0,0 +1,104 @@
+
+## 使用方法说明
+### 配置快应用开发环境
+#### 安装Node.jS
+需安装6.0以上版本的Node.jS，请从[Node.js官网](https://nodejs.org/en/)下载
+#### 通过git下载项目
+在命令行中执行以下命令下载项目：
+> git clone git@10.16.28.73:IT-MobileDivision/huawei-quickapp.git  
+
+#### 手机安装调试器
+调试器是一个Android应用程序，下载[调试器APK](https://statres.quickapp.cn/quickapp/quickapp/201806/file/quickapp_debugger.apk)
+
+在手机上安装并打开调试器，按钮功能如下：
+
+* 扫码安装：配置HTTP服务器地址，下载rpk包，并唤起平台运行rpk包
+* 本地安装：选择手机文件系统中的rpk包，并唤起平台运行rpk包
+* 在线更新：重新发送HTTP请求，更新rpk包，并唤起平台运行rpk包
+* 开始调试：唤起平台运行rpk包，并启动远程调试  
+
+**注意**：若打开调试器无法点击按钮，请升级手机系统到最新版本或安装平台预览版
+
+安装成功后如下图所示：  
+
+![](https://doc.quickapp.cn/tutorial/getting-started/build-environment.png)
+
+### 运行项目
+#### 安装依赖
+在项目根目录下，运行如下命令安装模块到node_modules目录
+> npm install
+
+#### 编译  
+1、手动编译项目  
+
+在项目的根目录下，运行如下命令进行编译打包，生成rpk包  
+
+> npm run build  
+
+编译打包成功后，项目根目录下会生成文件夹：**build、dist**
+* build：临时产出，包含编译后的页面js，图片等
+* dist：最终产出，包含rpk文件。其实是将build目录下的资源打包压缩为一个文件，后缀名为rpk，这个rpk文件就是项目编译后的最终产出  
+
+2、自动编译项目  
+
+如果希望每次修改源代码文件后，都自动编译项目，请使用如下命令：  
+
+> npm run watch  
+
+#### 安装rpk包  
+
+编译项目产出了rpk包后，请打开手机调试器。
+
+若打开调试器无法点击按钮，请升级手机系统到最新版本或安装平台预览版。
+
+调试器可使用后，就可以在调试器上安装rpk包了。支持以下两种安装方式，开发者可根据场景选择其一
+
+##### 扫码安装  
+
+1、启动HTTP服务器
+
+在终端中新建一个窗口，进入项目的根目录运行如下命令，启动本地服务器（默认端口为12306）
+
+> npm run server  
+
+自定义端口（如：8080）
+
+> npm run server -- --port 8080  
+
+2、在手机上预览运行效果
+配置HTTP服务器地址有两种方式，以下两者选其一即可：
+
+* 打开调试器 --> 点击"扫码安装"，扫描终端窗口中的二维码即可完成配置（若扫描不成功，可在浏览器中打开页面：http://localhost:<your port>，扫描页面中的二维码）
+* 打开调试器 --> 点击右上角menu --> 设置，输入终端窗口中提示的HTTP服务器地址  
+  
+配置完成后，可点击在线更新唤起平台运行rpk包
+
+若提示安装失败，请检查执行npm run server的终端窗口是否正常运行
+
+##### 本地安装
+1、复制rpk包到手机中
+
+将<ProjectName>/dist目录下编译产出的rpk包通过USB数据线或其他方式，复制到手机文件系统中
+
+2、本地安装rpk包
+
+打开调试器 --> 点击"本地安装"，选择手机文件系统中的rpk包，并自动唤起平台运行rpk包，查看效果  
+
+#### 工程目录结构
+如图：
+
+![](http://oss-cn-foshan.midea.com:17480/userDownload/9131190D33C241D983103F1715E545FD/pic/1547533057%281%29.png?certification=v11daa88a340f9b2a6538098731f2b5a2121)
+
+（注释）
+目前快应用开发只在src目录下开发即可
+src结构：
+![](http://oss-cn-foshan.midea.com:17480/userDownload/9131190D33C241D983103F1715E545FD/pic/1547533323%281%29.png?certification=v11f54bef840818031d380a9f626e5290136)
+
+1、新增品类在src目录下添加即可，添加规则：新建文件夹——>文件夹命名（0x+品类名）——>新建一个index.ux文件；
+2、Common目录下即为公共信息，包括：js,css,图片等，其中api目录下的api.js文件即编写接口信息；
+3、Component目录下即为自定义公共组件；
+4、mainfest.json文件用来编写接口引入声明以及路由的定义等（具体说明请参考[快应用manifest.json说明](https://doc.quickapp.cn/framework/manifest.html)）；
+
+
+
+
---
+++ src/Common/api/api.js
@@ -0,0 +1,468 @@
+const host = "http://ov-sit.smartmidea.net";
+const url = "ws://ovws-sit.smartmidea.net";
+const appkeyForvivo = "c2db6cbc7609febb7c54f0df5234506c";
+const appkeyForoppo = "b37142d426c90ddc1b28d1182f363384";
+// const host = "https://ov-prod.smartmidea.net";
+// const url = "wss://ovws-sit.smartmidea.net";
+// const appkeyForvivo = "00498c1bb52ad5d4d81eca53dae47fd1";
+// const appkeyForoppo = "ddb2668091508e243cc3b9570d094164";
+import fetch from '@system.fetch';
+import device from '@system.device';
+import prompt from '@system.prompt';
+const appidForvivo  = 2150;
+const appidForoppo  = 2149;
+import websocketfactory from '@system.websocketfactory';
+import uuid from './uuid.js';
+import SHA from 'js-sha256';
+
+var Fly=require("flyio/dist/npm/hap")
+var fly=new Fly(fetch);
+
+const isvivo = false;  //配置vivo或者oppo环境
+
+let appid  = '';
+let appkey = '';
+device.getInfo({
+	success: function (ret) {
+		if(ret.brand === 'vivo'){
+			appid  = appidForvivo;
+			appkey = appkeyForvivo;
+		}else{
+			appid  = appidForoppo;
+			appkey = appkeyForoppo;
+		}
+	}
+})
+export default {
+	//api接口
+	hostData: {
+		language: '',
+		getUserToken     : '/v1/iotopen/user/token/get',
+		postDeviceControl: '/v1/iotopen/device/deviceControl',
+		postDeviceBind:'/v1/iotopen/device/bind',
+		postDeviceStatusQuery:'/v1/iotopen/device/statusQuery',
+		getConfigWorkHelp: "http://smartbbs.midea.com/msmart.php?mod=zhiyin&op=ovsearch",
+	},
+	//获取语言
+	getlanguage(){
+		let p = new Promise(function(resolve, reject){
+			device.getInfo({
+				success: function (ret) {
+					if(ret.language){
+						resolve(ret.language);
+					}
+				}
+			})
+		})
+		return p;
+	},
+	//快连规则
+	getUrlSSid(url){
+		let mmid = "&mode=1";
+		let mmid1 = '&mode=001';
+		let qrCode = '';
+		if(url.toLowerCase().indexOf(mmid) != -1 || url.toLowerCase().indexOf(mmid1) != -1){
+			let len = 0;
+			if(url.toLowerCase().indexOf(mmid) != -1){
+				len = url.toLowerCase().indexOf(mmid);
+			}else{
+				len = url.toLowerCase().indexOf(mmid1);
+			}
+			qrCode = url.substring(0,len);
+			if(qrCode.length === 22 || qrCode.length === 64 || qrCode.length === 65 || qrCode.length === 70 || qrCode.length === 69){
+				return "midea_ac_" + qrCode.substring(qrCode.length - 4, qrCode.length);
+			}else if (qrCode.length === 106 || qrCode.length === 109) {
+				return qrCode.substring(qrCode.length - 13, qrCode.length);
+			}else if (qrCode.length === 61){
+				return ("midea_" + qrCode.substring(48,50) + "_xxxx").toLowerCase();
+			}else{
+				return '';
+			}
+		}
+	},
+	//获取url参数
+	getUrlCan(url,key){
+　　　　let arrObj = url.split("?");
+　　　　if (arrObj.length > 1) {
+　　　　　　let arrPara = arrObj[1].split("&");
+　　　　　　let arr;
+　　　　　　for (let i = 0; i < arrPara.length; i++) {
+　　　　　　　　arr = arrPara[i].split("=");
+　　　　　　　　if (arr != null && arr[0] == key) {
+　　　　　　　　　　return arr[1];
+　　　　　　　　}
+　　　　　　}
+　　　　　　return false;
+　　　　}else {
+　　　　　　return false;
+　　　　}
+	},
+	//匹配设备品类
+	getDeviceInformation(str){
+		let that = this;
+		let deviceSSID = "";
+		let url_str = str.indexOf('kt_APP');
+		let mideaUrl = str.indexOf('midea_');
+		if(str.indexOf('kt_APP') > -1){
+			deviceSSID = '0xAC';
+			return deviceSSID;
+		}else if(str.indexOf('midea_') > -1){
+			deviceSSID = '0x' + str.substring(str.indexOf('midea_')+6,str.indexOf('midea_')+8);
+			return deviceSSID;
+		}else if(that.getUrlCan(str,'type')){
+			deviceSSID = '0x' + that.getUrlCan(str,'type').substring(4,6);
+			return deviceSSID;
+		}else if(that.getUrlCan(str,'SSID')){
+			deviceSSID = '0x' + that.getUrlCan(str,'SSID').substring(6,8);
+			return deviceSSID;
+		}else{
+			return false;
+		}
+	},
+	//生成UUID字符串
+	createUUID(){
+		let len = uuid.create().toString().length;
+		let s   = uuid.create().toString();
+		let str = s.substring(0,8) + s.substring(9,13) + s.substring(14,18) + s.substring(19,23) + s.substring(24,len)
+		return str;
+	},
+	//获取时间戳
+	getTimestamp(){
+		// let timestamp = (new Date()).getTime();
+		let timestamp = (new Date()).valueOf();
+		let num = new Number(timestamp);
+		return num.toString();
+	},
+	//获取消息签名
+	getSignature(str1,str2,str3){
+		let that = this;
+		let str  = 'appid=' + appid + "&nonce=" + str2 + "&timestamp=" + str3;
+		return SHA.sha256(str + str1 + appkey);
+	},
+	//获取设备绑定消息签名
+	getSignatureDeviceBind(str1,str2,str3,str4){
+		let that = this;
+		let str  = "accesstoken=" + str2 + '&appid=' + appid +  "&nonce=" + str3 + "&timestamp=" + str4;
+		return SHA.sha256(str + str1 + appkey);
+	},
+	//弹出框
+	showDialog(str){
+		prompt.showToast({
+			message:"请求失败："+JSON.stringify(str),
+			duration:1
+		})
+	},
+	//第三方平台用户获取访问令牌accessToken
+	getUserToken(params){
+		let that        = this;
+		let objStr      = '';
+		    params.type = 1;
+		if((typeof params) === 'object'){
+			objStr = JSON.stringify(params);
+		}
+		let strObj = {
+			"appId"       : appid,
+			"timestamp"   : that.getTimestamp(),
+			"nonce"       : that.createUUID(),
+			"Content-Type": 'application/json'
+		}
+		    strObj.signature = that.getSignature(objStr,strObj.nonce,strObj.timestamp)
+		var p                = new Promise(function(resolve, reject){
+			if((typeof params) === 'object'){
+				console.log("params:"+JSON.stringify(params))
+				console.log("strObj:"+JSON.stringify(strObj))
+				console.log("url:"+host + that.hostData.getUserToken)
+				fly.post(host + that.hostData.getUserToken, params, {headers:strObj}).then(function (response) {
+					response.code = response.status;
+					console.log("返回信息："+JSON.stringify(response));
+					resolve(response)
+				}).catch(function (error) {
+					reject(error);
+				});
+			}else{
+				let obj = {
+					errorCode: "0000",
+					msg      : "参数有误"
+				}
+				reject(obj)
+			}
+		});
+		return p;
+	},
+	//绑定设备
+	postDeviceBind(params,accessToken){
+		let that        = this;
+		let objStr      = '';
+		if((typeof params) === 'object'){
+			objStr = JSON.stringify(params);
+		}
+		let strObj = {
+			"appId"       : appid,
+			"timestamp"   : that.getTimestamp(),
+			"nonce"       : that.createUUID(),
+			'accessToken': accessToken,
+			"Content-Type": 'application/json'
+		}
+		    strObj.signature = that.getSignatureDeviceBind(objStr,strObj.accessToken,strObj.nonce,strObj.timestamp)
+		var p                = new Promise(function(resolve, reject){
+			if((typeof params) === 'object'){
+				console.log("params:"+JSON.stringify(params))
+				console.log("strObj:"+JSON.stringify(strObj))
+				console.log("url:"+host + that.hostData.postDeviceBind);
+				fly.post(host + that.hostData.postDeviceBind, params, {headers:strObj}).then(function (response) {
+					response.code = response.status;
+					resolve(response);
+					console.log("返回信息："+JSON.stringify(response));
+				}).catch(function (error) {
+					reject(error);
+				})
+			}else{
+				let obj = {
+					errorCode: "0000",
+					msg      : "参数有误"
+				}
+				reject(obj)
+			}
+		});
+		return p;
+	},
+	//设备控制接口
+	postDeviceControl(params,accessToken){
+		let that        = this;
+		let objStr      = '';
+		if((typeof params) === 'object'){
+			objStr = JSON.stringify(params);
+		}
+		console.log("请求开始时间戳："+that.getTimestamp())
+		let strObj = {
+			"appId"       : appid,
+			"timestamp"   : that.getTimestamp(),
+			"nonce"       : that.createUUID(),
+			'accessToken': accessToken,
+			"Content-Type": 'application/json'
+		}
+		    strObj.signature = that.getSignatureDeviceBind(objStr,strObj.accessToken,strObj.nonce,strObj.timestamp)
+		var p                = new Promise(function(resolve, reject){
+			if((typeof params) === 'object'){
+				console.log("params:"+JSON.stringify(params))
+				console.log("strObj:"+JSON.stringify(strObj))
+				console.log("url:"+host + that.hostData.postDeviceControl);
+				fly.post(host + that.hostData.postDeviceControl, params, {headers:strObj}).then(function (response) {
+					response.code = response.status;
+					console.log("返回信息："+JSON.stringify(response));
+					resolve(response);
+				}).catch(function (error) {
+					reject(error);
+				});
+			}else{
+				let obj = {
+					errorCode: "0000",
+					msg      : "参数有误"
+				}
+				reject(obj)
+			}
+		});
+		return p;
+	},
+	//设备状态查询
+	postDeviceStatusQuery(params,accessToken){
+		let that        = this;
+		let objStr      = '';
+		if((typeof params) === 'object'){
+			objStr = JSON.stringify(params);
+		}
+		let strObj = {
+			"appId"       : appid,
+			"timestamp"   : that.getTimestamp(),
+			"nonce"       : that.createUUID(),
+			'accessToken': accessToken,
+			"Content-Type": 'application/json'
+		}
+		    strObj.signature = that.getSignatureDeviceBind(objStr,strObj.accessToken,strObj.nonce,strObj.timestamp)
+		var p = new Promise(function(resolve, reject){
+			if((typeof params) === 'object'){
+				console.log("params:"+JSON.stringify(params))
+				console.log("strObj:"+JSON.stringify(strObj))
+				console.log("url:"+host + that.hostData.postDeviceStatusQuery)
+				fly.post(host + that.hostData.postDeviceStatusQuery, params, {headers:strObj}).then(function (response) {
+					response.code = response.status;
+					console.log("返回信息："+JSON.stringify(response));
+					resolve(response);
+				}).catch(function (error) {
+					console.log(JSON.stringify(error));
+					reject(error);
+				});
+			}else{
+				let obj = {
+					errorCode: "0000",
+					msg      : "参数有误"
+				}
+				reject(obj)
+			}
+		});
+		return p;
+	},
+	//获取配网指引信息
+	getConfigWorkHelp(params){
+		let that = this;
+		var p    = new Promise(function(resolve, reject){
+			if((typeof params) === 'object'){
+				fetch.fetch({
+					url    : that.hostData.getConfigWorkHelp,
+					data   : params,
+					method : 'GET',
+					success: function (data) {
+						console.log("配网指引:"+JSON.stringify(data))
+						resolve(data);
+					},
+					fail: function (data, code) {
+						reject(data + "code:" + code);
+					}
+				})
+			}else{
+				let obj = {
+					errorCode: "0000",
+					msg      : "参数有误"
+				}
+				reject(obj)
+			}
+		});
+		return p;
+	},
+	//websocket
+	// createWebsocket
+	createWensocket(deviceid){
+		let did = deviceid;
+		let p = new Promise(function(resolve, reject){
+			device.getInfo({
+				success: function (ret) {
+					if(ret.brand === 'vivo'){
+						appid  = appidForvivo;
+						appkey = appkeyForvivo;
+					}else{
+						appid  = appidForoppo;
+						appkey = appkeyForoppo;
+					}
+					let len = appkey.length;
+					let s = Math.floor(Math.random()*(len-12));
+// 					prompt.showToast({
+// 						message:"url:"+url + '/simplews/status/fetch?appId=' + appid + '&s=' + s + '&l=10&key=' + appkey.substring(s,s+10) + '&did=' + did
+// 					})
+							let ws = websocketfactory.create({
+								url: url + '/simplews/status/fetch?appId=' + appid + '&s=' + s + '&l=10&key=' + appkey.substring(s,s+10) + '&did=' + did,
+								header:{
+									'content-type': 'application/json'
+								},
+							})
+							resolve(ws)
+				}
+			})
+		})
+		return p;
+	},
+	//错误码code
+	getCode(code,msg){
+		let str = code.toString();
+		let data = '';
+		switch(str){
+			case '9999':
+				data = '系统错误'
+			break;
+			case '3001':
+				data = '请求的地址不存在'
+			break;
+			case '3002':
+				data = "请求的HTTP METHOD不支持，请检查是否选择了正确的POST/GET方式"
+			break;
+			case '3003':
+				data = "缺少必备参数"
+			break;
+			case '3004':
+				data = "参数值错误"
+			break;
+			case '3155':
+				data = "设备已被解除绑定"
+			break;
+			case '3156':
+				data = "refreshToken不存在"
+			break;
+			case '3157':
+				data = "refreshToken过期"
+			break;
+			case '3158':
+				data = "token过期"
+			break;
+			case '3159':
+				data = "token不存在"
+			break;
+			case '3138':
+				data = "设备控制超时，请重试"
+			break;
+			default:
+				data = msg+',请稍后重试'
+			break;
+		}
+		return data;
+	},
+	//错误码status
+	getStatus(type,code,msg){
+		let str = code.toString();
+		let data = '';
+		switch(str){
+			case '-1':
+				data = '设备已被解除绑定'
+			break;
+			case '-2':
+				data = '设备无此属性'
+			break;
+			case '-3':
+				data = "此属性是不可操作的"
+			break;
+			case '-4':
+				data = "无效的subscriptionId"
+			break;
+			case '-5':
+				data = "设备当前状态不支持此操作"
+			break;
+// 			case '-6':
+// 				data = "设备离线"
+// 			break;
+			case '-7':
+				data = "设备处于关闭状态"
+			break;
+			case '-8':
+				data = "控制指令不正确"
+			break;
+			case '-9':
+				if(type === 'get'){
+					data = "获取设备失败，请稍后重试"
+				}else{
+					data = "设备操作失败，请稍后重试"
+				}
+			break;
+			case '-100':
+				if(type === 'get'){
+					data = "获取设备失败，请稍后重试"
+				}else{
+					data = "设备操作失败，请稍后重试"
+				}
+			break;
+			default:
+				data = msg+',请稍后重试'
+			break;
+		}
+		return data;
+	}
+}
+
+//添加响应拦截器，响应拦截器会在then/catch处理之前执行
+fly.interceptors.response.use(
+    (response) => {
+        //只将请求结果的data字段返回
+        // return response.data
+    },
+    (err) => {
+			console.log("请求失败信息："+JSON.stringify(err));
+			// that.showDialog(err);
+    }
+)
